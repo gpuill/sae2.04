e6526c3c32e96404dad2c485ed6946639e1724a6
Rochpearse <116382781+gpuill@users.noreply.github.com>
2023-05-17 11:13:04 +0200

    Update base.sql

--- base.sql
+++ base.sql
@@ -4,20 +4,19 @@ create schema distribill;
 set schema 'distribill';
 
 
-# Attributs = GOOD
+-- Attributs = GOOD
 CREATE TABLE session(
   session_annee                     INTEGER     NOT NULL,
   CONSTRAINT session_pk PRIMARY KEY(session_annee)            
 );
 
-
-# Attributs = GOOD
+-- Attributs = GOOD
 CREATE TABLE region (
   region_nom                        VARCHAR     NOT NULL,
   CONSTRAINT region_pk PRIMARY KEY(region_nom)
 );
 
-# Attributs = GOOD
+-- Attributs = GOOD
 CREATE TABLE departement (
   departement_code                  VARCHAR     NOT NULL,
   departement_nom                   VARCHAR     NOT NULL,
@@ -25,7 +24,8 @@ CREATE TABLE departement (
   CONSTRAINT departement_pk PRIMARY KEY(departement_code,region_nom)
 );
 
-# Attributs = GOOD
+
+-- Attributs = GOOD
 CREATE TABLE commune(
   commune_nom                       VARCHAR     NOT NULL,
   departement_code                  VARCHAR     NOT NULL,
@@ -33,7 +33,7 @@ CREATE TABLE commune(
 );
 
 
-# Attributs = GOOD
+-- Attributs = GOOD
 CREATE TABLE etablissement(
   etablissement_code_uai            VARCHAR     NOT NULL,
   etablissement_nom                 VARCHAR     NOT NULL,
@@ -51,14 +51,15 @@ CREATE TABLE formation (
   url_formation                     VARCHAR     NOT NULL,
   tri                               VARCHAR     NOT NULL,
   commune_nom                       VARCHAR     NOT NULL,
-  com_departement_code              VARCHAR     NOT NULL,
+  departement_code                  VARCHAR     NOT NULL,
   filiere_id                        INTEGER     NOT NULL,
   academie_nom                      VARCHAR     NOT NULL,
   etablissement_code_uai            VARCHAR     NOT NULL,
   CONSTRAINT formation_pk PRIMARY KEY(cod_aff_form)
 );
 
-# Attributs = GOOD
+
+-- Attributs = GOOD
 CREATE TABLE filiere (
   filiere_id                        INTEGER     NOT NULL,
   filiere_libelle                   VARCHAR     NOT NULL,
@@ -70,25 +71,25 @@ CREATE TABLE filiere (
   CONSTRAINT filiere_pk PRIMARY KEY(filiere_id)
 );
 
-# Attributs = GOOD
+-- Attributs = GOOD
 CREATE TABLE academie(
   academie_nom                      VARCHAR     NOT NULL,
   CONSTRAINT academie_pk PRIMARY KEY(academie_nom)
 );
 
 
-# Attributs = GOOD
+-- Attributs = GOOD
 CREATE TABLE rang_dernier_appele_selon_regroupement(
   cod_aff_form                      VARCHAR     NOT NULL,
   session_annee                     INTEGER     NOT NULL,
-  libelle_regroupement              VARCHAR     NOT NULL,
+  libelle-regroupement              VARCHAR     NOT NULL,
   rang_dernier_appele_groupe1       INTEGER,
   rang_dernier_appele_groupe2       INTEGER,
   rang_dernier_appele_groupe3       INTEGER,
-  CONSTRAINT rang_dernier_appele_selon_regroupement_pk PRIMARY KEY(cod_aff_form,session_annee,libelle_regroupement)
+  CONSTRAINT rang_dernier_appele_selon_regroupement_pk PRIMARY KEY(cod_aff_form,session_annee,libelle-regroupement)
 );
 
-# Attributs = GOOD
+-- Attributs = GOOD
 CREATE TABLE regroupement(
   libelle_regroupement              VARCHAR     NOT NULL,
   regroupement_1                    INTEGER,
@@ -137,6 +138,61 @@ CREATE TABLE effectif_selon_mention(
     effectif_admis_neo_bac_selon_mention_type_mention_tres_bien_fel INTEGER NOT NULL,
     CONSTRAINT effectif_selon_mention_pk PRIMARY KEY(libelle_mention,cod_aff_form,session_annee)
 );
+
+ALTER TABLE departement 
+ADD CONSTRAINT departement_fk_region 
+FOREIGN KEY(region_nom) 
+references region(region_nom);
+
+ALTER TABLE commune 
+ADD CONSTRAINT commune_fk_departement
+FOREIGN KEY(departement_code) 
+references departement(departement_code);
+
+ALTER TABLE formation 
+ADD CONSTRAINT formation_fk_commune 
+FOREIGN KEY(commune_nom,departement_code) 
+references commune(commune_nom,departement_code);
+
+ALTER TABLE formation 
+ADD CONSTRAINT formation_fk_etablissement 
+FOREIGN KEY(etablissement_code_uai) 
+references etablissement(etablissement_code_uai);
+
+ALTER TABLE formation 
+ADD CONSTRAINT formation_fk_academie 
+FOREIGN KEY(academie_nom) 
+references academie(academie_nom);
+
+ALTER TABLE formation 
+ADD CONSTRAINT formation_fk_filiere
+FOREIGN KEY(filiere_id) 
+references filiere(filiere_id);
+
+ALTER TABLE admissions_selon_type_neo_bac 
+ADD CONSTRAINT admissions_selon_type_neo_bac_fk_formation
+FOREIGN KEY(cod_aff_form) 
+references formation(cod_aff_form);
+
+ALTER TABLE admissions_selon_type_neo_bac 
+ADD CONSTRAINT admissions_selon_type_neo_bac_fk_type_bac
+FOREIGN KEY(type_bac) 
+references type_bac(type_bac);
+
+ALTER TABLE admissions_selon_type_neo_bac 
+ADD CONSTRAINT admissions_selon_type_neo_bac_fk_session
+FOREIGN KEY(session_annee) 
+references session(session_annee);
+
+ALTER TABLE admissions_generalistes 
+ADD CONSTRAINT admissions_generalistes_fk_session
+FOREIGN KEY(session_annee) 
+references session(session_annee);
+
+ALTER TABLE admissions_generalistes 
+ADD CONSTRAINT admissions_generalistes_fk_formation
+FOREIGN KEY(cod_aff_form) 
+references formation(cod_aff_form);
 
 ALTER TABLE departement 
 ADD CONSTRAINT departement_fk_region 
